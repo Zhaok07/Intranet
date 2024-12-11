4e1dde0f4dc6a4e7517447955ffdcf8ed80527ca
Caio Caetano Marques <caiocaetanomarques@gmail.com>
2024-12-11 14:09:19 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import PaginaHome from './Home/PaginaHome.jsx'
-import PaginaPop from './POP/PaginaPop.jsx'
-import PaginaRamal from './Ramal/PaginaRamal.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>, // Componente principal que pode incluir um Layout
-  },
-
-  {
-    path: "/pop",
-
-    element: <PaginaPop/>
-  },
-  {
-    path: "/home",
-
-    element: <PaginaHome/>
-  },
-  {
-    path: "/ramal",
-    element: <PaginaRamal/>
-  }
-],
-);
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import PaginaHome from './Home/PaginaHome.jsx';
+import PaginaPop from './POP/PaginaPop.jsx';
+import PaginaRamal from './Ramal/PaginaRamal.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<PaginaHome />} />
+        <Route path="/pop" element={<PaginaPop />} />
+        <Route path="/ramal" element={<PaginaRamal />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
